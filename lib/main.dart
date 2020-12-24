0d6917be12a47487f14752636486a13938bacfb5
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-24 16:38:47 +0530

    FLUT-3229 - Sample and screenshot added.

---
+++ lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(CellBuilderPicker());
+
+class CellBuilderPicker extends StatefulWidget {
+  @override
+  ViewNavigationState createState() => ViewNavigationState();
+}
+
+class ViewNavigationState extends State<CellBuilderPicker> {
+  DateRangePickerController _controller;
+
+  @override
+  initState() {
+    _controller = DateRangePickerController();
+
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Card(
+            margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+            child: SfDateRangePicker(
+              view: DateRangePickerView.month,
+              controller: _controller,
+              cellBuilder: cellBuilder,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget cellBuilder(
+      BuildContext context, DateRangePickerCellDetails cellDetails) {
+    if (_controller.view == DateRangePickerView.month) {
+      return Column(
+        children: [
+          Container(
+            child: Icon(
+              Icons.wb_sunny,
+              color: Colors.yellow,
+            ),
+          ),
+          Container(
+            child: Text(DateFormat('dd').format(cellDetails.date)),
+          )
+        ],
+      );
+    } else if (_controller.view == DateRangePickerView.year) {
+      return Column(
+        children: [
+          Container(
+            child: Icon(
+              Icons.wb_sunny,
+              color: Colors.yellow,
+            ),
+          ),
+          Container(
+            child: Text(DateFormat('MMM').format(cellDetails.date)),
+          )
+        ],
+      );
+    } else if (_controller.view == DateRangePickerView.decade) {
+      return Column(
+        children: [
+          Container(
+            child: Icon(
+              Icons.wb_sunny,
+              color: Colors.yellow,
+            ),
+          ),
+          Container(
+            child: Text(DateFormat('yyy').format(cellDetails.date)),
+          )
+        ],
+      );
+    } else if (_controller.view == DateRangePickerView.century) {
+      final int yearValue = cellDetails.date.year;
+      return Column(
+        children: [
+          Container(
+            child: Icon(
+              Icons.wb_sunny,
+              color: Colors.yellow,
+            ),
+          ),
+          Container(
+            child:
+                Text(yearValue.toString() + ' - ' + (yearValue + 9).toString()),
+          )
+        ],
+      );
+    }
+  }
+}
