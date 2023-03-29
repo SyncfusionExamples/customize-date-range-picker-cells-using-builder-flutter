3786262198bd3256fbc874e4c3ebf902c6210d63
Muniappan <muniappan.subramanian@syncfusiopn.com>
2023-03-29 17:08:26 +0530

    sample updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
-void main() => runApp(CellBuilderPicker());
+void main() => runApp(const CellBuilderPicker());
 
 class CellBuilderPicker extends StatefulWidget {
+  const CellBuilderPicker({super.key});
+
   @override
   ViewNavigationState createState() => ViewNavigationState();
 }
@@ -12,6 +14,7 @@ class CellBuilderPicker extends StatefulWidget {
 class ViewNavigationState extends State<CellBuilderPicker> {
   final DateRangePickerController _controller = DateRangePickerController();
 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -35,59 +38,42 @@ class ViewNavigationState extends State<CellBuilderPicker> {
     if (_controller.view == DateRangePickerView.month) {
       return Column(
         children: [
-          Container(
-            child: Icon(
-              Icons.wb_sunny,
-              color: Colors.yellow,
-            ),
+          const Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
           ),
-          Container(
-            child: Text(DateFormat('dd').format(cellDetails.date)),
-          )
+          Text(DateFormat('dd').format(cellDetails.date))
         ],
       );
     } else if (_controller.view == DateRangePickerView.year) {
       return Column(
         children: [
-          Container(
-            child: Icon(
-              Icons.wb_sunny,
-              color: Colors.yellow,
-            ),
+          const Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
           ),
-          Container(
-            child: Text(DateFormat('MMM').format(cellDetails.date)),
-          )
+          Text(DateFormat('MMM').format(cellDetails.date))
         ],
       );
     } else if (_controller.view == DateRangePickerView.decade) {
       return Column(
         children: [
-          Container(
-            child: Icon(
-              Icons.wb_sunny,
-              color: Colors.yellow,
-            ),
+          const Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
           ),
-          Container(
-            child: Text(DateFormat('yyy').format(cellDetails.date)),
-          )
+          Text(DateFormat('yyy').format(cellDetails.date))
         ],
       );
     } else {
       final int yearValue = cellDetails.date.year;
       return Column(
         children: [
-          Container(
-            child: Icon(
-              Icons.wb_sunny,
-              color: Colors.yellow,
-            ),
+          const Icon(
+            Icons.wb_sunny,
+            color: Colors.yellow,
           ),
-          Container(
-            child:
-                Text(yearValue.toString() + ' - ' + (yearValue + 9).toString()),
-          )
+          Text('$yearValue - ${yearValue + 9}')
         ],
       );
     }
